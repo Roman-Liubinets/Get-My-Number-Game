f22ca24282957520688a239e2c7e1b0c4af8a38a
Roman.Liubinets <roman.liubinets@gmail.com>
2017-01-18 18:40:58 +0200

    відстеження скільки в гравця є спроб

--- GMNG.rb
+++ GMNG.rb
@@ -9,3 +9,9 @@ puts "Welcome #{input}!"
 #Store a random number for the player guess.
 puts "I've got a random number between 1 and 100" "Can you guess it?"
 target = rand(100) + 1
+
+#Track how many guesses the player has made.
+num_guesses = 0
+puts "You've got #{10 - num_guesses} guesses left."
+print "Make a guess: "
+guess = gets.to_i
