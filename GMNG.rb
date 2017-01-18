49156d1d226cc6212ca5d2805bea88a06679b876
Roman.Liubinets <roman.liubinets@gmail.com>
2017-01-18 18:34:22 +0200

    появляння рандомного номеру

--- GMNG.rb
+++ GMNG.rb
@@ -4,3 +4,8 @@
 puts "Welcome to 'Get My Number!'"
 print "What's your name? "
 input = gets.chomp
+puts "Welcome #{input}!"
+
+#Store a random number for the player guess.
+puts "I've got a random number between 1 and 100" "Can you guess it?"
+target = rand(100) + 1
